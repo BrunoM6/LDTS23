5ebc75dd45d99ba2233c51bedac20e90409b4a0e
BrunoM6 <bfmbruno2003@gmail.com>
2023-11-21 20:12:26 +0000

    Main game loop

--- minesweeper/src/main/java/App.java
+++ minesweeper/src/main/java/App.java
@@ -1,4 +1,12 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 public class App {
-    MinesweeperGame minesweeper = new MinesweeperGame();
-
+    MinesweeperGame game = new MinesweeperGame(8, 8, 10);
+    Terminal terminal = new DefaultTerminalFactory().createTerminal();
+    Screen screen = new Screen(terminal);
+    while(true){
+        game.draw(screen);
+        game.handleInput(screen);
+    }
 }

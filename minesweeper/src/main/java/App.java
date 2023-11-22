c19ad9fd6f7ff7583586b09b666dc92a11815b66
Henrique <henriqueproenca2004@hotmail.com>
2023-11-22 15:44:23 +0000

    Adicionada função para por fonte createscreen e create terminal

--- minesweeper/src/main/java/App.java
+++ minesweeper/src/main/java/App.java
@@ -1,7 +1,9 @@
+import java.awt.*;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException, FontFormatException {
         Game game  = new Game();
         game.run();
     }
--- minesweeper/src/main/java/Game.java
+++ minesweeper/src/main/java/Game.java
@@ -1,26 +1,53 @@
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Game {
-    private Screen screen;
-    private Board board;
-    public Game() throws IOException {
+    private final Screen screen;
+    private final Board board;
+    public Game() throws IOException, URISyntaxException, FontFormatException {
         int col, row, mines;
-        col = 40;
-        row = 40;
+        col = 9;
+        row = 9;
         mines = 10;
+        board = new Board(col,row,mines);
+        AWTTerminalFontConfiguration fontConfig = loadFont();
+        Terminal terminal = createTerminal(col, row, fontConfig);
+        screen = createScreen(terminal);
+    }
+    private Screen createScreen(Terminal terminal) throws IOException{
+        final Screen screen1;
+        screen1 = new TerminalScreen(terminal);
+        screen1.startScreen();
+        screen1.doResizeIfNecessary();
+        return screen1;
+    }
+    private Terminal createTerminal(int col,int row ,AWTTerminalFontConfiguration fontConfig) throws IOException {
         TerminalSize terminalSize = new TerminalSize(col, row);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
-        screen = new TerminalScreen(terminal);
-        screen.startScreen();
-        screen.doResizeIfNecessary();
-        board = new Board(col,row,mines);
+        return terminal;
+    }
+    private AWTTerminalFontConfiguration loadFont() throws URISyntaxException, FontFormatException, IOException {
+        URL resource = getClass().getClassLoader().getResource("fonts/square.ttf");
+        File fontFile = new File(resource.toURI());
+        Font font = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.registerFont(font);
+        Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+        AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
+        return fontConfig;
     }
     private void draw() throws IOException {
         screen.clear();
@@ -32,6 +59,8 @@ public class Game {
         boolean run = true;
         while(run){
             draw();
+            KeyStroke key = screen.readInput();
+            if(key.getKeyType() == KeyType.EOF)run = false;
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
